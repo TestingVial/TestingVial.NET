36f0b116078806a74887d10bbf585de442824a48
Davide Bellone <davide.bellone@outlook.com>
2026-01-30 22:24:27 +0100

    Minor improvements

--- TestingVial.Examples/CartServiceTests.cs
+++ TestingVial.Examples/CartServiceTests.cs
@@ -2,49 +2,6 @@
 
 namespace TestingVial.Examples
 {
-
-    public record Cart(int Id);
-
-    public class Product
-    {
-        public string Name { get; init; }
-        public decimal Price { get; init; }
-    }
-    public class CartService
-    {
-        private Dictionary<Cart, List<Product>> carts = new();
-
-        public void AddToCart(Cart cart, Product product, int quantity)
-        {
-            if (carts.TryGetValue(cart, out var products))
-            {
-                for (int i = 0; i < quantity; i++)
-                {
-                    products.Add(product);
-                }
-            }
-            else
-            {
-                var productList = new List<Product>();
-                for (int i = 0; i < quantity; i++)
-                {
-                    productList.Add(product);
-                }
-                carts[cart] = productList;
-            }
-        }
-
-        public decimal GetCartTotal(Cart cart)
-        {
-            if (carts.TryGetValue(cart, out var products))
-            {
-                return products.Sum(p => p.Price);
-            }
-            return 0;
-        }
-
-    }
-
     [UnitTestVial<CartVial>(Description = "Carts can have multiple products added")]
     public class CartServiceTests
     {
